ba2e8582ee97ae1b30f3c041a02e6f421b8d12fd
Joan <joan1@Joans-iMac.local>
2014-11-20 12:54:50 +0100

    I build file test1.R

--- test1.R
+++ test1.R
@@ -1,5 +1,12 @@
 # from shell terminal do: 
 # git init
+# add file to do version control
+# git add -A
 
+#to commit changes
+#git commit -m "I build file test1.R"
 
+#git remote add origin https://github.com/jlopezmoliner/Test.git
 
+#to updload to github
+#git push -u origin master  
